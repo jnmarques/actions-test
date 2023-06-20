42c9fb4a1d832e07906a0750b01878e812d698e3
jnmarques <zecanelsonpm@gmail.com>
2023-06-20 11:25:27 +0100

    POC soft killing process through file descriptor

--- src/poc_decorator_monitor.py
+++ src/poc_decorator_monitor.py
@@ -1,13 +1,17 @@
 from calendar import c
+from math import e
 import time
 from decorators.monitored import monitored
 
 @monitored(common_name="test_it_out")
-def print_time(threadName, counter, delay):
-   while counter:
-      time.sleep(delay)
-      print("%s: %s" % (threadName, time.ctime(time.time())))
-      counter -= 1
+def print_time(threadName, counter, delay, event):
+    while counter:
+        if(event.is_set()):
+            print("Exiting gracefully")
+            exit(0)
+        time.sleep(delay)
+        print("%s: %s" % (threadName, time.ctime(time.time())))
+        counter -= 1
 
-print_time("Thread-1", 5, 1)
+print_time("Thread-1", 100, 1)
     
